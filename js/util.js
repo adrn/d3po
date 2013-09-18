599490d7c9d3538884d407fce4317d7beb8a1e01
Adrian Price-Whelan <adrian.prw@gmail.com>
2013-09-17 20:54:57 -0400

    moved array unique into util.js

---
+++ js/util.js
@@ -0,0 +1,12 @@
+// Return a unique version of an array
+Array.prototype.unique = function(){
+   var u = {}, a = [];
+   for(var i = 0, l = this.length; i < l; ++i){
+      if(u.hasOwnProperty(this[i])) {
+         continue;
+      }
+      a.push(this[i]);
+      u[this[i]] = 1;
+   }
+   return a;
+}
