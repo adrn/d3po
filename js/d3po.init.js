ed22a6b1203abd56e907f4cf9ba89b145ca8192d
Adrian Price-Whelan <adrian.prw@gmail.com>
2013-09-23 22:17:39 -0400

    restructuring

---
+++ js/d3po.init.js
@@ -0,0 +1,34 @@
+/*
+    Define some default style parameters
+*/
+
+// all size / padding parameters are specified in pixels
+var figureSpec = {  "figurePadding" : { "top" : 0, 
+                                        "left" : 50, 
+                                        "right" : 10, 
+                                        "bottom" : 40},
+                    "plotSpacing" : { "vertical" : 50,
+                                      "horizontal" : 100},
+                    "plotSize" : {"width" : 200,
+                                  "height" : 200}
+                 }
+
+var figPadding = figureSpec["figurePadding"],
+    plotSpacing = figureSpec["plotSpacing"],
+    plotSize = figureSpec["plotSize"]; 
+
+/*
+    These are some global variables we use below
+*/
+var currentState, 
+    jsonData, 
+    csvData;
+
+// Scalers for x / y axes from data space to pixel space
+var xScaler = d3.scale.linear()
+                .range([plotSpacing['horizontal']/2, 
+                        plotSize['width'] + plotSpacing['horizontal']/2]);
+
+var yScaler = d3.scale.linear()
+                .range([plotSize['height'] + plotSpacing['vertical']/2,
+                        plotSpacing['vertical']/2]);
--- js/d3po.js
+++ js/d3po.js
@@ -1,40 +1,3 @@
-/*
-    Define some default style parameters
-    
-    (TODO: add other ones here...)
-*/
-
-// all size / padding parameters are specified in pixels
-var figureSpec = {  "figurePadding" : { "top" : 0, 
-                                        "left" : 40, 
-                                        "right" : 20, 
-                                        "bottom" : 35},
-                    "plotSpacing" : { "vertical" : 40,
-                                      "horizontal" : 60},
-                    "plotSize" : 250
-                 }
-
-
-var figPadding = figureSpec["figurePadding"],
-    plotSpacing = figureSpec["plotSpacing"],
-    plotSize = figureSpec["plotSize"]; 
-
-/*
-    These are some global variables we use below
-*/
-var currentState, 
-    jsonData, 
-    csvData;
-
-// Scalers for x / y axes from data space to pixel space
-var xScaler = d3.scale.linear()
-                .range([plotSpacing['horizontal']/2, 
-                        plotSize - plotSpacing['horizontal']/2]);
-
-var yScaler = d3.scale.linear()
-                .range([plotSize - plotSpacing['vertical']/2,
-                        plotSpacing['vertical']/2]);
-
 function readD3POCSV(filename) {
     /* 
         TODO
@@ -43,9 +6,10 @@ function readD3POCSV(filename) {
         csvData = data;
         update_state(jsonData['states'][0]);
         
-        d3.select("#controls").selectAll("button").data(jsonData['states'])
+        d3.select("#controls").selectAll("button")
+            .data(jsonData['states'])
             .enter().append("button")
-            .html(function(d, i){ return d['name']; })
+            .text(function(d, i){ return d['name']; })
             .on("click", function(e,i) {
                 update_state(jsonData['states'][i]);
             });
