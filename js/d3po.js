f0b07e271a52265b4c292176a80c8b674acd4662
Adrian Price-Whelan <adrian.prw@gmail.com>
2013-09-29 01:43:14 -0400

    ::evil laugh:: starting to work on transitions

--- js/d3po.js
+++ js/d3po.js
@@ -1,21 +1,56 @@
-function readD3POCSV(filename) {
-    /* 
-        TODO
-    */
-    d3.csv(filename, function(error, data) {
-        csvData = data;
-        update_state(jsonData['states'][0]);
-        
-        d3.select("#controls .navigation").selectAll("li")
-            .data(jsonData['states'])
-            .enter().append("li")
-            .text(function(d, i){ return d['name']; })
-            .on("click", function(e,i) {
-                update_state(jsonData['states'][i]);
-                $(".navigation li").removeClass("selected");
-                $(this).addClass("selected");
-            });
+// Globals
+var jsonFilename, 
+    jsonData,
+    allPlotData;
 
-        $(".navigation li:first-of-type").addClass("selected");
+var svg;
+
+function initialize() {
+}
+
+function loadJSON(jsonFilename) {
+    /* 
+        Initialize the D3PO figure given a JSON specification.
+
+        Parameters
+        ----------
+        jsonFilename : string
+            The relative path to a D3PO JSON spec file.
+    */
+    d3.json(jsonFilename, function(error, _tmp) {
+        if (error) {
+            console.warn(error);
+            alert('Unable to find file "' + jsonFilename + '"');
+        }
+        jsonData = _tmp;
+
+        // TODO: figure out type of file, use appropriate reader
+        d3.csv(jsonData["filename"], function(error, _tmp) {
+            if (error) {
+                console.warn(error);
+                alert('Unable to read/parse file "' + jsonData["filename"] + '"');
+            }
+            allPlotData = _tmp;
+
+            // draw the first state
+            drawState(jsonData['states'][0]);
+
+            var presets = d3.select("#controls .navigation").selectAll("li")
+                            .data(jsonData['states']);
+
+            presets.enter().append("li")
+                   .on("click", function(e,i) {
+                        update_state(jsonData['states'][i]);
+                        $(".navigation li").removeClass("selected");
+                        $(this).addClass("selected");
+                   });
+
+            presets.text(function(d, i){ return d['name']; });
+            presets.exit().remove();
+
+            $(".navigation li:first-of-type").addClass("selected");
+        });
     });
+
+    //
 }
