bc3ed84d0fb698d49b8b9d4f19972b18d612370a
Adrian Price-Whelan <adrian.prw@gmail.com>
2013-12-15 18:34:24 -0500

    Add github client

--- d3po/__init__.py
+++ d3po/__init__.py
@@ -1,4 +1,8 @@
 from flask import Flask
+from .utils import GithubClient
+
 app = Flask(__name__)
+github_client = GithubClient()
+github_client.authenticate()
 
 import views
--- d3po/utils.py
+++ d3po/utils.py
@@ -8,6 +8,9 @@ __author__ = "adrn <adrn@astro.columbia.edu>"
 import os, sys
 import re
 
+# Third party
+import requests
+
 __all__ = ['get_gist_number']
 
 # stolen from iPython's nbviewer
@@ -25,3 +28,37 @@ def get_gist_number(value):
         return int(gist_url.group(2))
 
     raise ValueError("Invalid gist url or number.")
+
+class GithubClient(object):
+    github_api_url = 'https://api.github.com'
+
+    def __init__(self):
+        pass
+
+    def authenticate(self):
+        auth = {'client_id': os.environ.get('GITHUB_OAUTH_KEY', ''),
+                'client_secret': os.environ.get('GITHUB_OAUTH_SECRET', ''),
+                'token' : os.environ.get('GITHUB_API_TOKEN', '')}
+        self.auth = {k:v for k,v in auth.items() if v}
+
+    def github_api_request(self, path, params=None):
+        """ Make a GitHub API request to URL
+
+            URL is constructed from url and params, if specified.
+        """
+
+        url = "{}/{}".format(self.github_api_url, path)
+
+        params = {} if params is None else params
+        params.setdefault('user_agent', 'GitHub-Client')
+
+        if self.auth:
+            params.update(self.auth)
+
+        return requests.get(url, params=params)
+
+    def get_gist(self, gist_id):
+        """ Get a gist """
+
+        path = u'gists/{}'.format(gist_id)
+        return self.github_api_request(path)
