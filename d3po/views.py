dd74033274e9eafa0927ed5230752e6e86d57cd0
Adrian Price-Whelan <adrian.prw@gmail.com>
2013-12-15 17:19:42 -0500

    styled front page

--- d3po/views.py
+++ d3po/views.py
@@ -1,5 +1,17 @@
+# coding: utf-8
+
+from __future__ import division, print_function
+
+__author__ = "adrn <adrn@astro.columbia.edu>"
+
+# Standard library
+
+# Third-party
+from flask import render_template
+
+# Project
 from . import app
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    return render_template('index.html')
