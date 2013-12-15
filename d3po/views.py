5a3766b4612ef2dcb8256872c65d342910017e7c
Adrian Price-Whelan <adrian.prw@gmail.com>
2013-12-15 18:34:45 -0500

    start working on d3po views

--- d3po/views.py
+++ d3po/views.py
@@ -5,12 +5,13 @@ from __future__ import division, print_function
 __author__ = "adrn <adrn@astro.columbia.edu>"
 
 # Standard library
+import json
 
 # Third-party
 from flask import request, render_template, redirect, url_for
 
 # Project
-from . import app
+from . import app, github_client
 from .utils import *
 
 @app.route('/')
@@ -24,5 +25,16 @@ def create():
 
 @app.route('/gist/<int:gist_number>')
 def gist(gist_number):
-    print(gist_number)
+    resp = github_client.get_gist(gist_number)
+    resp_json = json.loads(resp.text)
+
+    for fname in resp_json['files'].keys():
+        xx,ext = os.path.splitext(fname)
+        content = resp_json['files'][fname]['content']
+
+        if ext == ".csv":
+            csv_data = content
+        elif ext == ".json":
+            json_states = content
+
     return ""
